c0742c37423db304d417f0e93716735974d580cd
Zoltan Patai <patai.zoltan@gmail.com>
2017-02-28 18:34:06 +0100

    adding new car implemented

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -2,4 +2,5 @@ class Brand < ApplicationRecord
 validates :brand_name, uniqueness: true
 validates :car_query_id, uniqueness: true
 has_many :models, dependent: :destroy
+has_many :cars, through: :models, dependent: :destroy
 end
--- app/models/car.rb
+++ app/models/car.rb
@@ -3,5 +3,5 @@ belongs_to :user
 belongs_to :model
 validates :model_id, presence: true
 validates :user_id, presence: true
-
+accepts_nested_attributes_for :model
 end
