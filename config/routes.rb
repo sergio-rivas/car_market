a7247581ac28668c1d438af9fa07d2b0230068cf
Zoltan Patai <patai.zoltan@gmail.com>
2017-02-28 15:39:18 +0100

    Facebook login added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :cars do
-		end
+    end
   resources :resources do
-		end
-  devise_for :users
+    end
+  devise_for :users,
+    controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
