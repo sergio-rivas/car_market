3e7e86bb6f1ebfaed869852b2bedf71f27a768b1
Zoltan Patai <patai.zoltan@gmail.com>
2017-02-28 12:24:51 +0100

    Seeded again

--- db/schema.rb
+++ db/schema.rb
@@ -15,11 +15,27 @@ ActiveRecord::Schema.define(version: 20170228110916) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "bookings", force: :cascade do |t|
+    t.datetime "start_time"
+    t.datetime "end_time"
+    t.integer  "length"
+    t.integer  "car_id"
+    t.index ["car_id"], name: "index_bookings_on_car_id", using: :btree
+  end
+
   create_table "brands", force: :cascade do |t|
     t.string   "car_query_id"
     t.string   "brand_name"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+  end
+
+  create_table "cars", force: :cascade do |t|
+    t.string "name"
+  end
+
+  create_table "resources", force: :cascade do |t|
+    t.string "name"
   end
 
   create_table "users", force: :cascade do |t|
@@ -39,4 +55,5 @@ ActiveRecord::Schema.define(version: 20170228110916) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
 
+  add_foreign_key "bookings", "cars"
 end
