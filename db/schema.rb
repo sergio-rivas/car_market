bff5c50922e68813884fed3e8231b78ece343bbe
Sergio Rivas <sergiorivas@163.com>
2017-03-01 09:17:11 +0100

    ?

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170228143600) do
 
-  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "brands", force: :cascade do |t|
