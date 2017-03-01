cc0e1675d37264dae7a4a7159810b2a98f269c74
Sergio Rivas <sergiorivas@163.com>
2017-03-01 18:02:27 +0100

    fixing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -6,9 +6,9 @@
 //= require_tree .
 
 
- $(function(){
-      $('#car_brands_brand_id').change(function(){
-        var brand_text = $('#car_brands_brand_id').find(":selected").text();
+ jQuery(function(){
+      jQuery('#car_brands_brand_id').change(function(){
+        var brand_text = jQuery('#car_brands_brand_id').find(":selected").text();
 
         document.location.href = '/cars/new?brand_name='+brand_text;
       });
