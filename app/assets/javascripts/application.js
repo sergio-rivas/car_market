dc6e5501231002bc802967b080844d93bfa9cd8c
Sergio Rivas <sergiorivas@163.com>
2017-03-01 17:58:16 +0100

    fixing

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,10 +1,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require jquery.ui.widget
-//= require jquery.iframe-transport
-//= require jquery.fileupload
-//= require cloudinary/jquery.cloudinary
+
 //= require attachinary
 //= require_tree .
 
