8e5d0f3c7fc868280106d14423d2ca8fa4b11b56
Zoltan Patai <patai.zoltan@gmail.com>
2017-03-02 12:16:52 +0100

    authentication fixed

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,6 @@
 class CarsController < ApplicationController
   before_action :set_car, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   # GET /cars
   def index
