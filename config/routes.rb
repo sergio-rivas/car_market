7a3da89b9b4e53687226f8a021bcf2d976ab9fe5
Zoltan Patai <patai.zoltan@gmail.com>
2017-03-09 16:43:35 +0100

    new car controller and forms fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,23 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :cars do
+    collection do
+      get "vin", to: "cars#vin"
+      post "vin", to: "cars#create_from_vin"
+    end
+
     resources :appointments, only: [:create]
     resources :messages, only: [:create]
   end
   resources :users, only: [:show] do
     resources :appointments, only: [:destroy]
   end
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }, path_prefix: 'd'
   mount Attachinary::Engine => "/attachinary"
 
-  get '/cars/new/2' => 'cars#new2', :as => :new_cars_2
-  post '/cars/new' => 'cars#create_via_vin'
+  # get '/cars/new/2' => 'cars#new2', :as => :new_cars_2
+  # post '/cars/new' => 'cars#create_via_vin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
