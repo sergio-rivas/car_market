cedcf6fee39e61c12e87b4c2bdb6e2136c6f4100
Zoltan Patai <patai.zoltan@gmail.com>
2017-03-15 12:23:59 +0100

    price needs to be added

--- app/models/car.rb
+++ app/models/car.rb
@@ -6,6 +6,7 @@ class Car < ApplicationRecord
   belongs_to :model
   validates :model_id, presence: true
   validates :user_id, presence: true
+  validates :price, presence: true
   accepts_nested_attributes_for :model
   has_attachments :photos, maximum: 5
 
